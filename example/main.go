2aefe82e9edd1d4cfeb0a6adf3ab83771f3f9610
fire-burn@yandex.ru <sidelnikov>
2025-01-14 10:43:10 +0300

    add example

--- example/main.go
+++ example/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	notifyLS "github.com/Fast-IQ/notify-lock-session"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 func main() {
-	//
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGABRT)
+
+	chanClose := make(chan bool, 1)
+	end := make(chan bool, 1)
+
+	go func() {
+		for {
+			<-quit
+			fmt.Println("Exit. Wait close.")
+			chanClose <- true
+			end <- true
+			//other close operation
+		}
+	}()
+
+	info := make(chan notifyLS.Lock, 10)
+
+	_ = notifyLS.Subscribe(info, chanClose)
+
+	i := 0
+	for i == 0 {
+		select {
+		case l := <-info:
+			if l.Lock {
+				fmt.Println(l.Clock, "Session lock")
+			} else {
+				fmt.Println(l.Clock, "Session unlock")
+			}
+		case <-end:
+			i = 1
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module github.com/Fast-IQ/notify-lock-session.git
+module github.com/Fast-IQ/notify-lock-session
 
 go 1.23.3
 
-require github.com/jthmath/winapi v0.0.0-20220924150906-a0bf4d56a80d
+require (
+	github.com/godbus/dbus/v5 v5.1.0
+	github.com/jthmath/winapi v0.0.0-20220924150906-a0bf4d56a80d
+)
